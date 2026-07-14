054d1729abf8f4535a93923e5b916d33a570ea1a
mahet-muhet <238361342+mahet-muhet@users.noreply.github.com>
2026-07-14 18:01:59 +0600

    prepare projectile game files

--- src/main.c
+++ src/main.c
@@ -11,19 +11,22 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 
+#define SCREEN_WIDTH 1200
+
+#define SCREEN_HEIGHT 800
+
 int main ()
 {
 	// Tell the window to use vsync and work on high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
-
 	// Create the window and OpenGL context
-	InitWindow(800, 600, "Hello Raylib");
+	InitWindow(SCREEN_HEIGHT, SCREEN_WIDTH, "Orb-e");
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
-	SearchAndSetResourceDir("resources");
+	//SearchAndSetResourceDir("resources");
 
 	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
+	//Texture wabbit = LoadTexture("wabbit_alpha.png");
 	
 	// game loop
 	while (!WindowShouldClose())		// run the loop until the user presses ESCAPE or presses the Close button on the window
@@ -38,7 +41,7 @@ int main ()
 		DrawText("Hello Raylib", 200,200,20,WHITE);
 
 		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
+		//DrawTexture(wabbit, 400, 200, WHITE);
 		
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
@@ -46,7 +49,7 @@ int main ()
 
 	// cleanup
 	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
+	//UnloadTexture(wabbit);
 
 	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
--- src/projectile.c
+++ src/projectile.c
@@ -1,5 +1,23 @@
 #include "raylib.h"
 
+#define SCREEN_WIDTH 1200
+
+#define SCREEN_HEIGHT 800
+
+//projectile throw game loop
+
 int projectileThrow(){
+    Rectangle ground = {0,SCREEN_HEIGHT/2, SCREEN_WIDTH, SCREEN_HEIGHT/2};
+    int end_projectile_game = 0;
+    while (!WindowShouldClose && !end_projectile_game){
+
+        float deltaTime = GetFrameTime();
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawRectangleRec(ground, GRAY);
+
+
+
+    }
     
 }
