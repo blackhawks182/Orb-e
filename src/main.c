bab5499fe95a62ebe783b9fe21e50155017a07ba
Araf171 <arafarsham31@gmail.com>
2026-07-14 17:38:21 +0600

    Birth of orb

--- src/main.c
+++ src/main.c
@@ -3,6 +3,15 @@
 
 #define SCREEN_WIDTH 1200
 #define SCREEN_HEIGHT 800
+#define SHIP_SIZE 20.0f
+#define MOVE_SPEED 350.0f // Speed of direct movement in pixels per second
+
+typedef struct Orb {
+    Vector2 position;
+    float radius;
+    float rotation; // Maintained purely to point the visual indicator where you move
+} Orb;
+
 
 int main(void) {
     // 1. Initialization
