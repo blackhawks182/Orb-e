fd90e2711768333a56723b4a287ebb97234353c8
mahet-muhet <238361342+mahet-muhet@users.noreply.github.com>
2026-07-15 08:23:45 +0600

    implement placeholder text for collider

--- src/collider.c
+++ src/collider.c
@@ -6,5 +6,11 @@
 
 void startCollider(void)
 {
-    BeginDrawing();
+    int textWidth = MeasureText("Coming Soon!", 40);
+    while(!WindowShouldClose() && !IsKeyPressed(KEY_ZERO)){
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText("Coming Soon!", SCREEN_WIDTH/2 - textWidth/2, SCREEN_HEIGHT/2, 40, RAYWHITE);
+        EndDrawing();
+    }
 }
