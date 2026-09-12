00cdc1dc9e796ccba5fcd3d0cccf4a5227fb06dc
mahet-muhet <238361342+mahet-muhet@users.noreply.github.com>
2026-09-12 18:49:52 +0600

    Jump and move

--- src/collider.c
+++ src/collider.c
@@ -18,6 +18,10 @@
 
 #define speedSpeed 500.0f
 
+#define jumpSpeed 500.0f
+
+#define gravity 1000.0f
+
 #define startSpeed 100.0f
 
 #define startMass 1.0f
@@ -47,11 +51,28 @@ void startCollider(void)
 
         if (IsKeyDown(KEY_D) && Orb.velocity.x < maxSpeed) Orb.velocity.x += speedSpeed * deltaTime;
         if (IsKeyDown(KEY_A) && Orb.velocity.x > minSpeed) Orb.velocity.x -= speedSpeed * deltaTime;
-        if (IsKeyPressed(KEY_SPACE) && !Orb.inAir) Orb.inAir = 1;
+        if (IsKeyPressed(KEY_SPACE) && !Orb.inAir){
+            Orb.inAir = 1;
+            Orb.velocity.y = -jumpSpeed;
+        }
+        Orb.position.x += Orb.velocity.x * deltaTime;
+
+        if (Orb.inAir){
+            Orb.velocity.y += gravity * deltaTime;
+            Orb.position.y += Orb.velocity.y * deltaTime;
+
+            if (Orb.position.y + colliderOrb_size >= SCREEN_HEIGHT*2/3){
+                Orb.position.y = SCREEN_HEIGHT*2/3 - colliderOrb_size;
+                Orb.velocity.y = 0;
+                Orb.inAir = 0;
+            }
+        }
+
         BeginDrawing();
         ClearBackground(BLACK);
         DrawRectangleRec(ground, GRAY);
         DrawCircleV(Orb.position, colliderOrb_size, RED);
+        DrawText(TextFormat("Speed: %.0f", Orb.velocity.x), Orb.position.x - 50, Orb.position.y - 50, 20, WHITE);
         EndDrawing();
     }    
 }
