db4ee23e831dcf51de23ca131cf9656ca9f02288
mahet-muhet <238361342+mahet-muhet@users.noreply.github.com>
2026-09-14 03:59:08 +0600

    Make header file for cannon throw

--- src/cannon_throw.c
+++ src/cannon_throw.c
@@ -1,26 +1,4 @@
-#include "raylib.h"
-
-#include "raymath.h"
-
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
-
-#define SCREEN_WIDTH 1200
-
-#define SCREEN_HEIGHT 800
-
-#define CANNON_POSITION 120
-
-#define thetaSpeed 200.0f
-
-#define projectileOrb_size 15.0f
-
-#define maxSpeed 1000.0f
-
-#define speedSpeed 1666.6f
-
-#define minSpeed 0.0f
-
-#define gravity 600.0f
+#include "cannon_throw.h"
 
 Vector2 rightEdgeMidpoint(Rectangle rect, Vector2 origin, float rotation)
 {
@@ -37,20 +15,6 @@ Vector2 rightEdgeMidpoint(Rectangle rect, Vector2 origin, float rotation)
 // Runs the game loop and switches between game states.
 
 void startCannonThrow(void){
-    typedef struct projectileOrb{
-        Vector2 position;
-        Vector2 velocity;
-        int inAir;
-    } projectileOrb;
-
-    typedef struct obstacle{
-    Rectangle rectangle;
-    Vector2 velocity;
-    int type;
-    int active;
-    float leftbound;
-    float rightbound;
-    } obstacle;
 
     obstacle obstacles[10];
 
